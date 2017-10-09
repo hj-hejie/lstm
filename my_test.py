d053be893a1f731c8c45a03057c4280ca66ad11e
bottomheater <bottomheater94@gmail.com>
2017-10-09 09:42:13 +0800

    guoshui

--- my_test.py
+++
@@ -1,7 +0,0 @@
-import my_debugger
-
-debugger = my_debugger.debugger()
-debugger.load("C:\\WINDOWS\\system32\\calc.exe")
-#pid=raw_input("Enter the PID of the processto attach to: ")
-#debugger.attach(int(pid))
-#debugger.detach()
